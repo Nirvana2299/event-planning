2280c592490a40414afddf55034a856952b05859
Nirvana <shoaibarms@gmail.com>
2023-04-27 20:08:27 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles.css'
 import Navbar from './Components/Navbar'
 import Carousel from './Components/Carousel';
+import Planning from './Components/Planning';
 
 function App() {
   return (
     <div>
-    <Navbar/>
-    <Carousel/>
+      <Navbar />
+      <Carousel />
+      <Planning />
     </div>
   );
 }
