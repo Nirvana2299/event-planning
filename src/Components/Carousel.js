0bec13716aae88b266926073d23d9d2ddb9331db
Nirvana <shoaibarms@gmail.com>
2023-04-27 21:26:12 +0530

    modified:   src/Components/Carousel.js

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -46,7 +46,7 @@ export default function Carousel() {
     <div className='max-w-fit m-auto relative group backdrop-blur-sm bg-white/30 left-0 right-0' >
     <div className="carousel">
     <div>
-      <img className='image h-132 w-screen object-fill'  src={images[currentIndex].url} alt={`${currentIndex}`} />
+      <img className='image h-132 w-screen object-cover'  src={images[currentIndex].url} alt={`${currentIndex}`} />
       </div>
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <button onClick={previousImage}>Previous</button>
