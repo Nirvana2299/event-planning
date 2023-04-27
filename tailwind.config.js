39d61da8761d34f8f8092e06545c453de50b765d
Nirvana <shoaibarms@gmail.com>
2023-04-27 17:22:36 +0530

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '128': '32rem',
+        '132': '44rem',
+        '136': '48rem'
+      }
+    },
   },
   plugins: [],
 }
