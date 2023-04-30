7a62d149101c96ecfddb931f07905ae666292b30
Nirvana <shoaibarms@gmail.com>
2023-04-30 18:34:48 +0530

    new file:   src/Components/About.js

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+
+
+export default function About() {
+    return (
+        <div>
+            <div className="flex justify-center flex-col text-center py-16 px-4 screen-h">
+                <h1 className="text-4xl subpixel-antialiased cursive font-bold break-all">A better workflow</h1>
+                <p className="pt-6">
+                    Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam
+                    eget aliquam. Quisque id at vitae feugiat egestas.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+
+    )
+}
