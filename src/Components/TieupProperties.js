26ab47644d8430ce7bab4464c090c5c4f2d774a4
Nirvana <shoaibarms@gmail.com>
2023-04-30 23:24:20 +0530

    modified:   src/Components/TieupProperties.js

--- src/Components/TieupProperties.js
+++ src/Components/TieupProperties.js
@@ -32,19 +32,19 @@ export default function TieupProperties() {
         <p className="text-4xl subpixel-antialiased cursive font-bold">Our Tieup Properties</p>
       </div>
       <div className="flex flex-row py-16 px-4 justify-center flex-wrap">
-        <div className='w-auto h-96 object-filled pr-6 pl-6' >
+        <div className='w-auto h-96 object-filled pr-6 pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
           <img className='rounded-lg h-80 w-80 object-cover' src={images[0].url} alt='image1' />
           <h3 className="flex justify-center mt-2">Green House</h3>
         </div>
-        <div className='w-auto h-96 object-filled pr-6 pl-6' >
+        <div className='w-auto h-96 object-filled pr-6 pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
           <img className='rounded-lg h-80 w-80 object-cover' src={images[1].url} alt='image1' />
           <h3 className="flex justify-center mt-2">Lake Placid</h3>
         </div>
-        <div className='w-auto h-96 object-filled pr-6 pl-6' >
+        <div className='w-auto h-96 object-filled pr-6 pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
           <img className='rounded-lg h-80 w-80 object-cover' src={images[2].url} alt='image1' />
           <h3 className="flex justify-center mt-2">Green Gables Wedding Estate</h3>
         </div>
-        <div className='w-auto h-96 object-filled pr-6 pl-6' >
+        <div className='w-auto h-96 object-filled pr-6 pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
           <img className='rounded-lg h-80 w-80 object-cover' src={images[3].url} alt='image1' />
           <h3 className="flex justify-center mt-2">BentonVille</h3>
         </div>
