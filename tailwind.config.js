87bf15af7b557b9d3cec23017d52da98994331eb
Nirvana <shoaibarms@gmail.com>
2023-04-30 18:34:01 +0530

    modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,45 @@ module.exports = {
         'cursive': ['Dancing Script', 'cursive']
       },
       colors: {
-        'bisque': 'ffe4c4'
+        'bisque': {
+          100: '#ffe4c4',
+          200: '#ccb69d',
+          300: '#a3927e',
+          400: '#827565',
+          500: '#685e51',
+          600: '#534b41',
+          700: '#423c34',
+          800: '#35302a',
+          900: '#2a2622',
+          1000: '#221e1b'
+        },
+        'rosegold': {
+          100: '#b76e79'
+        },
+        'gentlecare': {
+          100: '#ffc3a0',
+          200: '#ffafbd'
+        }
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
