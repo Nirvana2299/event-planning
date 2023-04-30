0fe688e12c0df98e9277b6388514c5870d538fc8
Nirvana <shoaibarms@gmail.com>
2023-04-30 18:35:03 +0530

    new file:   src/Components/AboutAndForm.js

---
+++ src/Components/AboutAndForm.js
@@ -0,0 +1,34 @@
+
+import Form from './Form'
+
+export default function AboutAndForm() {
+  return (
+    <div className="overflow-hidden bg-bisque py-16 sm:py-16">
+      <div className="mx-auto max-w-7xl px-8 lg:px-2">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg ">
+              <h1 className="font cursive text-4xl text-center font-bold tracking-tight text-gray-900 sm:text-6xl sm:text-left subpixel-antialiased">
+                Summer styles are finally here
+              </h1>
+              <p className="mt-2 cursive text-4xl text-center font-bold tracking-tight text-gray-900 sm:text-4xl sm:text-left subpixel-antialiased">A better workflow</p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                iste dolor cupiditate blanditiis ratione.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                iste dolor cupiditate blanditiis ratione.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                iste dolor cupiditate blanditiis ratione.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+          {/* form part*/}
+        <Form />
+        </div>
+      </div>
+    </div>
+    
+  )
+}
