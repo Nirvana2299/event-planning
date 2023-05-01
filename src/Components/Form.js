c7cc831918c5b0dba15c3e3ae6d139905eea58e8
Nirvana <shoaibarms@gmail.com>
2023-05-02 01:04:00 +0530

    modified:   src/Components/Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -11,15 +11,15 @@ export default function About() {
     return (
         <div className="bg-dark py-4 px-8 sm:py-8 lg:px-4 border-dashed border-2 border-gentlecare-100">
             <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl cursive font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-                <p className="mt-2 text-lg leading-8 text-gray-600">
+                <h2 className="text-3xl cursive font-bold tracking-tight text-bisque-900 sm:text-4xl">Contact sales</h2>
+                <p className="mt-2 text-lg leading-8 text-bisque-600">
                     Aute magna irure deserunt veniam aliqua magna enim voluptate.
                 </p>
             </div>
             <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div>
-                        <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-bisque-900">
                             First name
                         </label>
                         <div className="mt-2.5">
@@ -28,12 +28,12 @@ export default function About() {
                                 name="first-name"
                                 id="first-name"
                                 autoComplete="given-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-bisque-900 shadow-sm ring-1 ring-inset ring-bisque-300 placeholder:text-bisque-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div>
-                        <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-bisque-900">
                             Last name
                         </label>
                         <div className="mt-2.5">
@@ -42,12 +42,12 @@ export default function About() {
                                 name="last-name"
                                 id="last-name"
                                 autoComplete="family-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-bisque-900 shadow-sm ring-1 ring-inset ring-bisque-300 placeholder:text-bisque-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="email" className="block text-sm font-semibold leading-6 text-bisque-900">
                             Email
                         </label>
                         <div className="mt-2.5">
@@ -56,12 +56,12 @@ export default function About() {
                                 name="email"
                                 id="email"
                                 autoComplete="email"
-                                className="block w-full bg-dark rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
+                                className="block w-full bg-dark rounded-md border-0 px-3.5 py-2 text-bisque-900 shadow-sm ring-1 ring-inset ring-bisque-300 placeholder:text-bisque-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-bisque-900">
                             Phone number
                         </label>
                         <div className="relative mt-2.5">
@@ -70,12 +70,12 @@ export default function About() {
                                 name="phone-number"
                                 id="phone-number"
                                 autoComplete="tel"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-bisque-900 shadow-sm ring-1 ring-inset ring-bisque-300 placeholder:text-bisque-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="message" className="block text-sm font-semibold leading-6 text-bisque-900">
                             Message
                         </label>
                         <div className="mt-2.5">
@@ -83,7 +83,7 @@ export default function About() {
                                 name="message"
                                 id="message"
                                 rows={4}
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-bisque-900 shadow-sm ring-1 ring-inset ring-bisque-300 placeholder:text-bisque-400 focus:ring-2 focus:ring-inset focus:ring-bisque-600 sm:text-sm sm:leading-6"
                                 defaultValue={''}
                             />
                         </div>
@@ -94,8 +94,8 @@ export default function About() {
                                 checked={agreed}
                                 onChange={setAgreed}
                                 className={classNames(
-                                    agreed ? 'bg-bisque-500' : 'bg-gray-200',
-                                    'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bisque-600'
+                                    agreed ? 'bg-bisque-500' : 'bg-bisque-200',
+                                    'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-bisque-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bisque-600'
                                 )}
                             >
                                 <span className="sr-only">Agree to policies</span>
@@ -103,12 +103,12 @@ export default function About() {
                                     aria-hidden="true"
                                     className={classNames(
                                         agreed ? 'translate-x-3.5' : 'translate-x-0',
-                                        'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
+                                        'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-bisque-900/5 transition duration-200 ease-in-out'
                                     )}
                                 />
                             </Switch>
                         </div>
-                        <Switch.Label className="text-sm leading-6 text-gray-600">
+                        <Switch.Label className="text-sm leading-6 text-bisque-600">
                             By selecting this, you agree to our{' '}
                             <a href="#" className="font-semibold text-bisque-900">
                                 privacy&nbsp;policy
