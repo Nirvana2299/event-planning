aa82830528b6611455c5ab7078905cd70c126d08
Nirvana <shoaibarms@gmail.com>
2023-05-03 01:23:02 +0530

    modified:   src/Components/AboutAndForm.js

--- src/Components/AboutAndForm.js
+++ src/Components/AboutAndForm.js
@@ -8,11 +8,11 @@ export default function AboutAndForm() {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg ">
-              <h1 className="font cursive text-4xl text-center font-bold tracking-tight text-gray-900 sm:text-6xl sm:text-left subpixel-antialiased">
+              <h1 className="font cursive text-5xl text-center font-bold tracking-tight text-bisque-900 sm:text-6xl sm:text-left subpixel-antialiased">
                 About Us
               </h1>
-              <p className="mt-2 cursive text-4xl text-center font-bold tracking-tight text-bisque-900 sm:text-4xl sm:text-left subpixel-antialiased">A better workflow</p>
-              <p className="mt-6 text-lg text-center leading-8 text-bisque-600 sm:text-lg sm:text-left">
+              <p className="mt-2 cursive text-3xl text-center font-bold tracking-tight text-bisque-900 sm:text-4xl sm:text-left subpixel-antialiased">A better workflow</p>
+              <p className="mt-6 text-xl text-center leading-8 text-bisque-600 sm:text-lg sm:text-left">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
                 iste dolor cupiditate blanditiis ratione.
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
