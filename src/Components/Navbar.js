cd8c7c32890fd54d1b15ff2d5b7b66d4cabe79af
Nirvana <shoaibarms@gmail.com>
2023-05-03 00:04:07 +0530

    modified:   src/Components/Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -34,7 +34,7 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-bisque backdrop-blur-3xl bg-bisque-100/30">
+    <header className="bg-bisque backdrop-blur-3xl bg-bisque-100/80">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
