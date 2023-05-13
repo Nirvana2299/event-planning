866bc92700007ce3525de67ed82f89068e1a8348
Nirvana <shoaibarms@gmail.com>
2023-05-13 15:52:49 +0530

    modified:   src/Components/TieupProperties.js

--- src/Components/TieupProperties.js
+++ src/Components/TieupProperties.js
@@ -28,7 +28,7 @@ export default function TieupProperties() {
       <div className="flex justify-center pt-16">
         <p className="sm:text-4xl text-5xl text-bisque-900 subpixel-antialiased cursive font-bold">Our Tieup Properties</p>
       </div>
-      <div className="flex flex-row py-16 px-4 justify-center flex-wrap">
+      <div className="flex flex-row py-16 px-1 sm:px-4 justify-center flex-wrap">
         <div className='w-auto h-52 sm:h-96 object-filled pr-2 pl-2 md:pr-6 pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
           <img className='rounded-lg h-40 w-40 sm:h-80 sm:w-80 object-cover' src={images[0].url} alt='image1' />
           <h3 className="flex justify-center mt-2 text-bisque-600 text-xl leading-8">Green House</h3>
