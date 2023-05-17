47c0a8940327ae3c18e9c9633a9ba5a5c84d24cf
Nirvana <shoaibarms@gmail.com>
2023-05-18 03:33:21 +0530

    modified:   src/Components/Line.js

--- src/Components/Line.js
+++ src/Components/Line.js
@@ -1,7 +1,7 @@
 export default function Line() {
     return (
         <div>
-            <div className="px-12 sm:px-20">
+            <div className="px-12 sm:px-6">
                 <hr className="border-bisque-600 border-solid"></hr>
             </div>
         </div>
