9044ac5cf27def01dc8d3c95bf6d65ba3d7d9502
Nirvana <shoaibarms@gmail.com>
2023-05-18 11:46:45 +0530

    modified:   src/Components/AboutAndForm.js

--- src/Components/AboutAndForm.js
+++ src/Components/AboutAndForm.js
@@ -3,7 +3,7 @@ import Form from './Form'
 
 export default function AboutAndForm() {
   return (
-    <div className="overflow-hidden bg-bisque py-16 sm:py-16">
+    <div className="overflow-hidden bg-bisque py-8 sm:py-16 sm:py-16">
       <div className="mx-auto max-w-7xl px-8 lg:px-2">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
