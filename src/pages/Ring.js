1ecfd7f50663ab0d0c1eeacffac73f366ccd4d83
Nirvana <shoaibarms@gmail.com>
2023-05-18 11:38:36 +0530

    new file:   src/pages/Ring.js

---
+++ src/pages/Ring.js
@@ -0,0 +1,9 @@
+export default function Ring() {
+    return (
+            <div className="pt-28 text-white bg-black">
+                <h1>
+                    Ring
+                </h1>
+            </div>
+    )
+}
