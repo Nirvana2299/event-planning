cdff408fe5a364af15ea789b96ac80396750c8cc
Nirvana <shoaibarms@gmail.com>
2023-05-18 11:38:05 +0530

    new file:   src/pages/Dine.js

---
+++ src/pages/Dine.js
@@ -0,0 +1,9 @@
+export default function Dine() {
+    return (
+            <div className="pt-28 sm:pt-26 text-white bg-black">
+                <h1>
+                    Dine
+                </h1>
+            </div>
+    )
+}
