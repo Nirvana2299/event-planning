bf748f0d051b2fbe245e4f7b61e06decaa34cf6e
Nirvana <shoaibarms@gmail.com>
2023-05-18 22:19:58 +0530

    modified:   src/Components/Portfolio.js

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,60 +1,95 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState, useEffect } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline'
+
+const imgURL1 =
+  'https://images.unsplash.com/photo-1519225421980-715cb0215aed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
+const imgURL2 =
+  'https://images.unsplash.com/photo-1469371670807-013ccf25f16a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
+const imgURL3 =
+  'https://images.unsplash.com/photo-1522673607200-164d1b6ce486?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
 
 const callouts = [
   {
     name: 'Desk and Office',
     description: 'Work from home accessories',
-    imageSrc: 'https://images.unsplash.com/photo-1519225421980-715cb0215aed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    imageSrc: {
+      image: [imgURL1, imgURL2, imgURL3],
+    },
     imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
     href: '#',
   },
   {
     name: 'Self-Improvement',
     description: 'Journals and note-taking',
-    imageSrc: 'https://images.unsplash.com/photo-1469371670807-013ccf25f16a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    imageSrc: {
+      image: [imgURL2, imgURL1, imgURL3],
+    },
     imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
     href: '#',
   },
   {
     name: 'Travel',
     description: 'Daily commute essentials',
-    imageSrc: 'https://images.unsplash.com/photo-1522673607200-164d1b6ce486?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    imageSrc: {
+      image: [imgURL3, imgURL1, imgURL2],
+    },
     imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
     href: '#',
   },
-]
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function Portfolio() {
-  const [open, setOpen] = useState(false)
-  const [selectedCallout, setSelectedCallout] = useState(null)
+  const [open, setOpen] = useState(false);
+  const [selectedCallout, setSelectedCallout] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % callouts.length);
+    }, 4000);
+    return () => clearInterval(intervalId);
+  }, [currentIndex]);
 
   function switchOn(callout) {
-    setOpen(true)
-    setSelectedCallout(callout)
+    setOpen(true);
+    setSelectedCallout(callout);
+    setCurrentIndex(0);
   }
 
-
   return (
-    <div id='portfolioSection'>
+    <div id="portfolioSection">
       <div className="bg-bisque-100">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-8 sm:py-24 lg:max-w-none lg:py-32">
             <h2 className="text-3xl cursive font-bold text-bisque-900">Portfolio</h2>
             <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
               {callouts.map((callout) => (
-                <div key={callout.name} className="group relative" onClick={() => switchOn(callout)} >
+                <div key={callout.name} className="group relative" onClick={() => switchOn(callout)}>
                   <div className="relative h-80 w-full overflow-hidden rounded-lg bg-bisque-100 sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                    <img
-                      src={callout.imageSrc}
-                      alt={callout.imageAlt}
-                      className="h-full w-full object-cover object-center"
-                    />
+                    <Transition
+                      show={selectedCallout === callout}
+                      enter="transition-opacity duration-1000"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="transition-opacity duration-1000"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      className="absolute inset-0"
+                    >
+                      {(ref) => (
+                        <img
+                          ref={ref}
+                          src={callout.imageSrc.image[currentIndex]}
+                          alt={callout.imageAlt}
+                          className="h-full w-full object-cover object-center"
+                        />
+                      )}
+                    </Transition>
                   </div>
                   <h3 className="mt-6 text-sm text-bisque-500">
                     <a>
@@ -72,7 +107,7 @@ export default function Portfolio() {
       {/*PopUp section*/}
       <Transition.Root show={open} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={() => setOpen(false)}>
-        {/* Popup Animation  */}
+          {/* Popup Animation  */}
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -97,7 +132,7 @@ export default function Portfolio() {
                   leaveFrom="opacity-100 translate-y-0 md:scale-100"
                   leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
                 >
-                {/* Popedout Menu */}
+                  {/* Popedout Menu */}
                   <Dialog.Panel className="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
                     <div className="relative flex w-full items-center overflow-hidden bg-bisque-100 px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8 rounded-lg">
                       <button
@@ -112,7 +147,7 @@ export default function Portfolio() {
                       <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
                         <div className="aspect-w-3 aspect-h-4 rounded-lg overflow-hidden sm:col-span-2 lg:col-span-1">
                           <img
-                            src={selectedCallout.imageSrc}
+                            src={selectedCallout.imageSrc.image[currentIndex]}
                             alt={selectedCallout.imageAlt}
                             className="object-cover object-center"
                           />
@@ -130,7 +165,6 @@ export default function Portfolio() {
           </div>
         </Dialog>
       </Transition.Root>
-
     </div>
-  )
+  );
 }
