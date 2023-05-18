25356c3468f8d5869ad5f06b4401d49a3c4254f6
Nirvana <shoaibarms@gmail.com>
2023-05-18 11:38:27 +0530

    new file:   src/pages/Isle.js

---
+++ src/pages/Isle.js
@@ -0,0 +1,9 @@
+export default function Isle() {
+    return (
+            <div className="pt-28 text-white bg-black">
+                <h1>
+                    Isle
+                </h1>
+            </div>
+    )
+}
