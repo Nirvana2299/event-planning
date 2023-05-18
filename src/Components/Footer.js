ab4f5e26300fc4bba3bc7a3a6ce5a39209f3efd2
Nirvana <shoaibarms@gmail.com>
2023-05-19 01:37:03 +0530

    modified:   src/pages/Home.js

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="flex justify-items-center align-center py-8 border-dashed border-2 border-gentlecare-100">
+        
+            <h3 className="cursive text-center text-md sm:text-xl bg-gradient-to-r from-blue-600 via-rose-400 to-indigo-400 inline-block text-transparent bg-clip-text font-bold mx-auto">
+                Made with ❤️ by Shoaib
+            </h3>
+        </footer>
+    )
+} 
