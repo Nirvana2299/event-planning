8f098e6c13c2e92661bac0f42df8b202fae9e43f
Nirvana <shoaibarms@gmail.com>
2023-05-18 11:36:54 +0530

    new file:   src/pages/Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import Navbar from '../Components/Navbar'
+import Carousel from '../Components/Carousel';
+import Planning from '../Components/Planning';
+import TieupProperties from '../Components/TieupProperties';
+import AboutAndForm from '../Components/AboutAndForm';
+import Line from '../Components/Line';
+import Portfolio from '../Components/Portfolio';
+
+export default function Home() {
+    return (
+      <div>
+        <Carousel />
+        <Planning />
+        <Line />
+        <TieupProperties />
+        <Line />
+        <AboutAndForm />
+        <Line />
+        <Portfolio />
+        <Line />
+      </div>
+    );
+  }
