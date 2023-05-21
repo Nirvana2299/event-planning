2e2f64ab20bc1a6301872d2fbe143fc3d49bbf6c
Nirvana <shoaibarms@gmail.com>
2023-05-21 13:27:43 +0530

    modified:   src/pages/Ring.js

--- src/pages/Ring.js
+++ src/pages/Ring.js
@@ -1,6 +1,11 @@
 export default function Ring() {
     return (
-            <div className="pt-28 text-white bg-black">
+            <div className="pt-28 text-bisque-900">
+            <img 
+                src="https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            />
+            
+            
                 <h1>
                     Ring
                 </h1>
