631dd7cd58d3b1c8b25f2567c80e45169068cb62
Nirvana <shoaibarms@gmail.com>
2023-05-21 13:27:31 +0530

    modified:   src/Components/TieupProperties.js

--- src/Components/TieupProperties.js
+++ src/Components/TieupProperties.js
@@ -30,19 +30,19 @@ export default function TieupProperties() {
       </div>
       <div className="flex flex-row py-8 sm:py-16 px-1 sm:px-4 justify-center flex-wrap">
         <div className='w-auto h-auto pb-0.5 sm:h-96 object-filled pr-2 pl-2 md:pr-6 md:pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
-          <img className='rounded-lg h-40 w-40 sm:h-80 sm:w-80 object-cover' src={images[0].url} alt='image1' />
+          <img className='rounded-lg h-44 w-44 sm:h-80 sm:w-80 object-cover' src={images[0].url} alt='image1' />
           <h3 className="flex justify-center mt-2 text-bisque-600 text-xl leading-8">Green House</h3>
         </div>
         <div className='w-auto h-auto pb-0.5 sm:h-96 object-filled pr-2 pl-2 md:pr-6 md:pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
-          <img className='rounded-lg h-40 w-40 sm:h-80 sm:w-80 object-cover' src={images[1].url} alt='image1' />
+          <img className='rounded-lg h-44 w-44 sm:h-80 sm:w-80 object-cover' src={images[1].url} alt='image1' />
           <h3 className="flex justify-center mt-2 text-bisque-600 text-xl leading-8">Lake Placid</h3>
         </div>
         <div className='w-auto h-auto pb-0.5 sm:h-96 object-filled pr-2 pl-2 md:pr-6 md:pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
-          <img className='rounded-lg h-40 w-40 sm:h-80 sm:w-80 object-cover' src={images[2].url} alt='image1' />
+          <img className='rounded-lg h-44 w-44 sm:h-80 sm:w-80 object-cover' src={images[2].url} alt='image1' />
           <h3 className="flex justify-center mt-2 text-bisque-600 text-xl leading-8">Green Gables</h3>
         </div>
         <div className='w-auto h-auto pb-0.5 sm:h-96 object-filled pr-2 pl-2 md:pr-6 md:pl-6 transition ease-in-out delay-150 bg-bisque-100 hover:-translate-y-1 hover:scale-110 hover:bg-transparent duration-300' >
-          <img className='rounded-lg h-40 w-40 sm:h-80 sm:w-80 object-cover' src={images[3].url} alt='image1' />
+          <img className='rounded-lg h-44 w-44 sm:h-80 sm:w-80 object-cover' src={images[3].url} alt='image1' />
           <h3 className="flex justify-center mt-2 text-bisque-600 text-xl leading-8">BentonVille</h3>
         </div>
       </div>
