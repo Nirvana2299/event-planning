8f8db2ca51313d6b040153dc121ae2a4880ac180
Nirvana <shoaibarms@gmail.com>
2023-05-22 22:07:54 +0530

    new file:   src/services/AnnualFunction.js

---
+++ src/services/AnnualFunction.js
@@ -0,0 +1,9 @@
+export default function AnnualFunction() {
+    return (
+        <div className="pt-28 sm:pt-26">
+            <h1>
+                Annual Function
+            </h1>
+        </div>
+    )
+}
