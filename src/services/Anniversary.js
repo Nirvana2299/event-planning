2c27554f402e0030cdf4c65fd1197e6652c90af7
Nirvana <shoaibarms@gmail.com>
2023-05-22 22:07:43 +0530

    new file:   src/services/Anniversary.js

---
+++ src/services/Anniversary.js
@@ -0,0 +1,9 @@
+export default function Anniversary() {
+    return (
+        <div className="pt-28 sm:pt-26">
+            <h1>
+                Anniversary
+            </h1>
+        </div>
+    )
+}
