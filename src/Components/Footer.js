048448e53c685e27b04020788652489524c93f92
Nirvana <shoaibarms@gmail.com>
2023-05-25 18:43:02 +0530

    modified:   src/Components/Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer>
             
                 <div className="grid gap-4 grid-cols-1 md:grid-cols-3 bg-black text-white">
-                    <div className="w-auto md:text-center py-6 px-6">
+                    <div className="w-auto py-6 px-6">
                         <h3 className="font-bold">Quick Links</h3>
                         <ul className="text-left pt-4">
                             <li>Home</li>
@@ -12,11 +12,11 @@ export default function Footer() {
                             <li>Contact Us</li>
                         </ul>
                     </div>
-                    <div className="w-auto md:text-center py-6 px-6">
+                    <div className="w-auto py-6 px-6">
                         <h3 className="font-bold">Head Office</h3>
                         <p className="text-left pt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     </div>
-                    <div className="w-auto md:text-center py-6 px-6">
+                    <div className="w-auto py-6 px-6">
                         <h3 className="font-bold">Mobile & E-Mail</h3>
                         <ul className="text-left pt-4">
                             <li>987456321</li>
@@ -25,7 +25,7 @@ export default function Footer() {
                         </ul>
                     </div>
                 </div>
-                <div className="flex justify-items-center bg-black align-center py-4 border-dashed border-2 border-gentlecare-100" >
+                <div className="flex justify-items-center bg-black align-center py-2 border-dashed border-2 border-gentlecare-100" >
                     <h3 className="cursive text-center text-md sm:text-xl bg-gradient-to-r from-blue-600 via-rose-400 to-indigo-400 inline-block text-transparent bg-clip-text font-bold mx-auto">
                         Made with ❤️ by Shoaib
                     </h3>
