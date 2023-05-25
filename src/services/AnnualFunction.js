255f19acc0aa3ea648bf81ce05ca9b4c3a929217
Nirvana <shoaibarms@gmail.com>
2023-05-25 15:47:38 +0530

    deleted:    src/services/AnnualFunction.js

--- src/services/AnnualFunction.js
+++
@@ -1,9 +0,0 @@
-export default function AnnualFunction() {
-    return (
-        <div className="pt-28 sm:pt-26">
-            <h1>
-                Annual Function
-            </h1>
-        </div>
-    )
-}
