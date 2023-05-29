9105a7c53cde43d642c12053f27b2043f676eebd
Nirvana <shoaibarms@gmail.com>
2023-05-30 02:31:32 +0530

    modified:   src/Components/Carousel.js

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,10 +1,13 @@
-import { useEffect, useState } from 'react';
-import { IoArrowBackCircleSharp, IoArrowForwardCircleSharp } from 'react-icons/io5';
-import { useSwipeable } from 'react-swipeable';
+import { useEffect, useState } from "react";
+import {
+  IoArrowBackCircleSharp,
+  IoArrowForwardCircleSharp
+} from "react-icons/io5";
+import { useSwipeable } from "react-swipeable";
 
 export default function Carousel() {
   const images = [
-       {
+    {
       url:
         "https://images.unsplash.com/photo-1515934751635-c81c6bc9a2d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
     },
@@ -26,50 +29,72 @@ export default function Carousel() {
     }
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentImage, setCurrentImage] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentIndex((currentIndex + 1) % images.length);
+      setCurrentImage((currentImage + 1) % images.length);
     }, 4000);
     return () => clearInterval(intervalId);
-  }, [currentIndex, images.length]);
+  }, [currentImage, images.length]);
 
   const nextImage = () => {
-    setCurrentIndex((currentIndex + 1) % images.length);
+    setCurrentImage((currentImage + 1) % images.length);
   };
 
   const previousImage = () => {
-    setCurrentIndex(currentIndex === 0 ? images.length - 1 : currentIndex - 1);
+    setCurrentImage(currentImage === 0 ? images.length - 1 : currentImage - 1);
   };
 
   const handlers = useSwipeable({
     onSwipedLeft: nextImage,
     onSwipedRight: previousImage,
     preventDefaultTouchmoveEvent: true,
-    trackMouse: true,
+    trackMouse: true
   });
+
+  const slideStyles = {
+    transform: `translateX(-${currentImage * (100 / images.length)}%)`,
+    transition: "transform 0.8s ease-in-out",
+    display: "flex",
+    width: `${images.length * 100}%`,
+    zIndex: 0 
+  };
+
+  const imageStyles = {
+    flexBasis: `${100 / images.length}%`,
+    maxWidth: `${100 / images.length}%`
+  };
 
   return (
     <div
-      id='slideSection'
-      className='max-w-fit m-auto pt-[6.80rem] sm:pt-28 relative group left-0 right-0'
+      id="slideSection"
+      className="max-w-fit m-auto pt-[6.80rem] sm:pt-28 relative group left-0 right-0 overflow-hidden"
       {...handlers}
     >
-      <div className="carousel">   
+      <div className="carousel">
+        <div className="slides" style={slideStyles}>
+          {images.map((image, index) => (
+            <div key={index} className="slide" style={imageStyles}>
               <img
-                className='image h-80 w-screen sm:h-128 object-cover'
-                src={images[currentIndex].url}
-                alt={`${currentIndex}`}
+                className="image h-80 w-screen sm:h-128 object-cover"
+                src={image.url}
+                alt={`${index}`}
               />
+            </div>
+          ))}
         </div>
-        <div className='hidden md:group-hover:block absolute sm:top-[55%] top-[62%] -translate-x-0 translate-y-[-50%] rounded-full left-5 text-4xl sm:text-4xl md:text-6xl backdrop-blur bg-white-100/20 text-bisque-100 cursor-pointer'>
-          <IoArrowBackCircleSharp onClick={previousImage}></IoArrowBackCircleSharp>
+        <div className="hidden md:group-hover:block absolute sm:top-[58%] top-[62%] -translate-x-0 translate-y-[-50%] rounded-full left-5 text-4xl sm:text-4xl md:text-6xl backdrop-blur bg-white-100/20 text-bisque-100 cursor-pointer">
+          <IoArrowBackCircleSharp
+            onClick={previousImage}
+          ></IoArrowBackCircleSharp>
         </div>
-        <div className='hidden md:group-hover:block absolute sm:top-[55%] top-[62%] -translate-x-0 translate-y-[-50%] rounded-full right-5 text-4xl sm:text-4xl md:text-6xl backdrop-blur bg-white-100/20 text-bisque-100 cursor-pointer'>
-          <IoArrowForwardCircleSharp onClick={nextImage}></IoArrowForwardCircleSharp>
+        <div className="hidden md:group-hover:block absolute sm:top-[58%] top-[62%] -translate-x-0 translate-y-[-50%] rounded-full right-5 text-4xl sm:text-4xl md:text-6xl backdrop-blur bg-white-100/20 text-bisque-100 cursor-pointer">
+          <IoArrowForwardCircleSharp
+            onClick={nextImage}
+          ></IoArrowForwardCircleSharp>
         </div>
       </div>
-   
+    </div>
   );
 }
