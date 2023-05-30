831fec1f0d1fa176b75ac08293fc744a91536bfc
Nirvana <shoaibarms@gmail.com>
2023-05-30 15:34:52 +0530

    modified:   src/Components/AboutAndForm.js

--- src/Components/AboutAndForm.js
+++ src/Components/AboutAndForm.js
@@ -13,10 +13,10 @@ export default function AboutAndForm() {
               </h1>
               <p className="mt-2 cursive text-3xl text-center font-bold tracking-tight text-bisque-900 sm:text-4xl sm:text-left subpixel-antialiased">A better workflow</p>
               <p className="mt-6 text-xl text-justify leading-8 text-bisque-800 sm:text-2xl sm:text-left">
-                Gavyan Events has provided full service event management, entertainment, promotion and association management services. Over the years, we have developed a sound reputation within our industry for creativity, efficiency, ability to meet deadlines and flawless execution of events and for providing superior association services.
+                 Tyohaar Event has provided full service event management, entertainment, promotion and association management services. Over the years, we have developed a sound reputation within our industry for creativity, efficiency, ability to meet deadlines and flawless execution of Tyohaar Event and for providing superior association services.
               </p>
               <p className="mt-6 text-xl text-justify leading-8 text-bisque-800 sm:text-2xl sm:text-left">
-              Gavyan started with a purpose to provide ‘ONE STOP SOLUTIONS’ for all your Wedding- related worries. customer or staff, Gavyan Events assures to handle every part of it with finesse. We step in right at the stage of Idea Generation to its flawless execution. Here, you’ll find a platform that will be unique to you and your brand – a platform that will define your success. Current success stories at “Gavyan Events”
+               started with a purpose to provide ‘ONE STOP SOLUTIONS’ for all your Wedding- related worries. customer or staff,  Tyohaar Event assures to handle every part of it with finesse. We step in right at the stage of Idea Generation to its flawless execution. Here, you’ll find a platform that will be unique to you and your brand – a platform that will define your success. Current success stories at “ Tyohaar Event”
               </p>
             </div>
           </div>
