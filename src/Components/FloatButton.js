a50c658f8872cf6e9f1a0e9185f5b3d7f89ae835
Nirvana <shoaibarms@gmail.com>
2023-06-01 15:38:34 +0530

    modified:   src/Components/FloatButton.js

--- src/Components/FloatButton.js
+++ src/Components/FloatButton.js
@@ -1,6 +1,8 @@
 import { Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
-import { IoChatbubblesSharp, IoCloseCircleSharp, IoLogoWhatsapp, IoCallOutline } from 'react-icons/io5';
+import { IoMailOutline, IoCloseCircleSharp, IoLogoWhatsapp, IoCall, IoChatbubbleSharp } from 'react-icons/io5';
+import { FaInstagram } from 'react-icons/fa'
+import { IoLogoFacebook } from 'react-icons/io'
 
 const FloatButton = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,13 +14,13 @@ const FloatButton = () => {
   return (
     <div className="fixed bottom-4 right-4">
       <button
-        className="bg-transparent h-12 w-12 text-pink-700 rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 noSelect"
+        className="bg-transparent h-12 w-12 text-[#4A503D] rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 noSelect"
         onClick={toggleOptions}
       >
         {isOpen ? (
           <IoCloseCircleSharp className="h-full w-full" />
         ) : (
-          <IoChatbubblesSharp className="h-full w-full" />
+          <IoChatbubbleSharp className="h-full w-full" />
         )}
       </button>
       <Transition
@@ -32,12 +34,21 @@ const FloatButton = () => {
       >
         <div className="absolute bottom-16 left-0 w-44">
           {/* <div className="bg-white border border-gray-200 rounded-lg shadow-lg"> */}
-          <IoCallOutline className="bg-blue-500 h-12 w-12 text-black mb-2 py-2 px-2 rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">
+          <IoCall className="bg-blue-500 h-12 w-12 text-black mb-2 py-2 px-2 rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">
               Option 1
-            </IoCallOutline>
-            <IoLogoWhatsapp className="bg-[#77DD77] h-12 w-12 text-black py-2 px-2 rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">
+            </IoCall>
+            <IoMailOutline className="bg-[#8e8e93] h-12 w-12 text-black mb-2 py-2 px-2 rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">
+              Option 2
+            </IoMailOutline>
+            <IoLogoWhatsapp className="bg-[#77DD77] h-12 w-12 text-black mb-2 py-2 px-2 rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">
               Option 2
             </IoLogoWhatsapp>
+            <IoLogoFacebook className="bg-[#177BF2] h-12 w-12 text-black mb-2 py-2 px-2 rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">
+              Option 1
+            </IoLogoFacebook>
+            <FaInstagram className="bg-[#962fbf] h-12 w-12 text-black mb-2 py-2 px-2 rounded-full transform transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-100">
+              Option 2
+            </FaInstagram>
           {/* </div> */}
         </div>
       </Transition>
