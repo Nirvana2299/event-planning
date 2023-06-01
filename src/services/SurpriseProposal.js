549ed4164d0399ab29eaaed664904ba53b2a0d18
Nirvana <90575279+Nirvana2299@users.noreply.github.com>
2023-06-02 00:24:12 +0530

    Update SurpriseProposal.js

--- src/services/SurpriseProposal.js
+++ src/services/SurpriseProposal.js
@@ -48,7 +48,7 @@ export default function SurpriceProposal() {
                 <div className="mx-auto grid max-w-2xl grid-cols-1 lg:gap-x-8 gap-y-4 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 
                     <div className="col-span-2 lg:pr-10">
-                        <p className=" mt-6 text-xl text-center leading-8 text-bisque-700 sm:text-lg sm:text-left pb-4">
+                        <p className=" mt-6 text-xl text-justify leading-6 text-bisque-700 sm:text-lg sm:text-left pb-4">
                         Welcome to our enchanting venue, where dreams come true! Picture this: a breathtaking backdrop of sparkling city lights, fragrant roses adorning the path, and soft music serenading the air. As the sun sets, immerse yourself in pure romance. And amidst this ethereal ambiance, a surprise proposal awaits you, filling your heart with everlasting love. Let the magic unfold at our venue, where forever begins.
                         </p>
                         <img className='w-screen h-64 object-cover rounded-lg' src={image} />
